47355dcd8258d8444719461c9a5916e9cc8706e4
HyunshinPark0 <hyunshin24@hanyang.ac.kr>
2026-04-03 00:28:00 +0900

    티어 이미지 부분 구현

---
+++ app/src/main/java/com/jeepark/onestep/data/model/Tier.kt
@@ -0,0 +1,14 @@
+package com.jeepark.onestep.data.model
+
+//티어별 경험치 통
+
+const val TIER1_MAX = 15
+const val TIER2_MAX = 48
+const val TIER3_MAX = 120
+const val TIER4_MAX = 230
+const val TIER5_MAX = 400
+const val TIER6_MAX = 650
+const val TIER7_MAX = 1000
+//수정해야됨 26.06.02 12:56
+
+
--- app/src/main/java/com/jeepark/onestep/ui/screens/ProgressScreen.kt
+++ app/src/main/java/com/jeepark/onestep/ui/screens/ProgressScreen.kt
@@ -1,12 +1,64 @@
 package com.jeepark.onestep.ui.screens
 
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.gestures.snapping.SnapPosition
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.jeepark.onestep.R
+
 
 @Composable
 fun ProgressScreen(
     modifier: Modifier = Modifier,
     onNavigateToMain : () -> Unit
 ) {
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(Color.White)
+    ){
+        Column(modifier = Modifier
+            .height(92.dp)
+            .fillMaxWidth()
+            .padding(16.dp)) {
+            Image(
+                painter = painterResource(id = R.drawable.tier_test_image),
+                contentDescription = "현재 티어 족자 이미지",
+                modifier = Modifier
+                    .size(60.dp),
+                contentScale = ContentScale.Fit)
+            Row(modifier = Modifier
+                .fillMaxWidth()
+                .height(46.dp)){}
+        }
 
+        Column(modifier = Modifier
+            .fillMaxHeight()
+            .fillMaxWidth()){}
+    }
+    }
+
+@Preview
+@Composable
+private fun df() {
+    ProgressScreen(onNavigateToMain = {})
 }
+
