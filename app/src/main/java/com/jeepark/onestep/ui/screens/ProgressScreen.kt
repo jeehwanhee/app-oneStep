93ae787be0e7e073c738de7d718f3685cd9c16ea
HyunshinPark0 <hyunshin24@hanyang.ac.kr>
2026-04-03 17:29:32 +0900

    진척도 row 수정

--- app/src/main/java/com/jeepark/onestep/ui/screens/ProgressScreen.kt
+++ app/src/main/java/com/jeepark/onestep/ui/screens/ProgressScreen.kt
@@ -4,15 +4,20 @@ package com.jeepark.onestep.ui.screens
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.gestures.snapping.SnapPosition
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -46,13 +51,32 @@ fun ProgressScreen(
                 modifier = Modifier
                     .size(60.dp),
                 contentScale = ContentScale.Fit)
-            Column(modifier = Modifier
-                .weight(1f)
-                .fillMaxWidth()){}
+            Spacer(modifier = Modifier.width(16.dp))
+
+            Column(modifier = Modifier //프레임 16
+                .fillMaxHeight(),
+                verticalArrangement = Arrangement.SpaceBetween){
+                Column(modifier = Modifier //임시 경험치바 -> progress bar로 구현해야됨
+                    .height(16.dp)
+                    .fillMaxWidth()
+                    .background(Color.Green)) { }
+
+                Spacer(modifier = Modifier.height(16.dp))
+
+                Row(modifier = Modifier
+                    .fillMaxWidth(),
+                    horizontalArrangement = Arrangement.SpaceBetween){
+                    Text(text = "현재 경험치 120 / 300") // 유저 데이터와 연결 필요
+                    Text(text = "40%") //유저 데이터와 연결 필요
+                }
+            }
         }
         Column(modifier = Modifier
             .fillMaxHeight()
-            .fillMaxWidth()){}
+            .fillMaxWidth()
+            .padding(horizontal = 16.dp, vertical = 32.dp)){
+
+        }
     }
     }
 
